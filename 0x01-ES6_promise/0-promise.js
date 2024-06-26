335720f81d745d4cc634f3c1e599c29c5c122b84
petersudai <psudai@gmail.com>
2024-06-26 18:51:44 +0000

    Keep every promise you make and only make promises you can keep

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+	  const success = true;
+	  if (success) {
+      resolve('Success response');
+    } else {
+      reject(new Error('Failed response'));
+    }
+  });
+}
