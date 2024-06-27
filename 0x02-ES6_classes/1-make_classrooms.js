e1ab6a90ad70e0864526baa4e90f8cd3c793a01c
petersudai <psudai@gmail.com>
2024-06-27 20:32:48 +0000

    Let's make some classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,5 +2,5 @@ import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
   const roomSizes = [19, 20, 34];
-  return roomSizes.map(size => new ClassRoom(size));
+  return roomSizes.map((size) => new ClassRoom(size));
 }
