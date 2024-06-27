819d586ffcb7701d2b61a19abcbe3145f846bbf4
petersudai <psudai@gmail.com>
2024-06-27 19:46:19 +0000

    Vroom

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,29 @@
+const cloneSymbol = Symbol('clone');
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  [cloneSymbol]() {
+    return new this.constructor();
+  }
+
+  cloneCar() {
+    return this[cloneSymbol]();
+  }
+}
