545998f19385836b26a6c753d9990962e7be13e8
petersudai <psudai@gmail.com>
2024-06-28 12:07:47 +0000

    EVCar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,7 +1,5 @@
 import Car from './10-car';
 
-const cloneSymbol = Symbol('clone');
-
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
