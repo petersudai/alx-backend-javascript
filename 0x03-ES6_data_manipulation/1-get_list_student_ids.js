e50f43314faa5f807513538dd4fec6c1f8879297
petersudai <psudai@gmail.com>
2024-07-02 13:53:28 +0000

    More mapping

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,4 @@
 export default function getListStudentIds(array) {
-  if (!Array.isArray(array)) {
-    return [];
-  }
-
+  if (!Array.isArray(array)) return [];
   return array.map((student) => student.id);
 }
