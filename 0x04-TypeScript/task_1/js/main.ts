f42c6b93a381f67ec9809f5e5ed25f9acf25f458
petersudai <psudai@gmail.com>
2024-07-03 19:24:00 +0000

    Extending the Teacher class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,12 +7,6 @@ interface Teacher {
   [propName: string]: any;
 }
 
-const teacher3: Teacher = {
-  firstName: 'John',
-  fullTimeEmployee: false,
-  lastName: 'Doe',
-  location: 'London',
-  contract: false,
-};
-
-console.log(teacher3);
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
