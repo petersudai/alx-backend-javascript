acb87cfdf28fe69c811a6de50c16375e406e8b26
petersudai <81692851+petersudai@users.noreply.github.com>
2024-09-07 16:43:48 +0300

    Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -9,19 +9,25 @@ const app = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
 
   if (req.url === '/') {
+    // Respond to root with "Hello Holberton School!"
     res.statusCode = 200;
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
+    // Respond to /students with the list of students
     res.statusCode = 200;
     res.write('This is the list of our students\n');
     countStudents(database)
       .then((output) => {
-        res.end(output);
+        res.write(output); // Append the student information
+        res.end(); // End the response
       })
       .catch(() => {
+        // If the database cannot be loaded, handle the error
+        res.statusCode = 500;
         res.end('Cannot load the database');
       });
   } else {
+    // Handle any other undefined routes
     res.statusCode = 404;
     res.end('Not found');
   }
