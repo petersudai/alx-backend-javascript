d30ccea7a77ed6ee5ea7a70a267ac5e4867b8787
petersudai <81692851+petersudai@users.noreply.github.com>
2024-09-07 16:01:55 +0300

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
 // Display the initial message
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
 	const name = data.toString().trim();
-	console.log(`Your name is: ${name}`);
-	console.log('This important software is now closing');
+	process.stdout.write(`Your name is: ${name}\n`);
+	process.stdout.write('This important software is now closing\n');
 	process.exit();
 });
