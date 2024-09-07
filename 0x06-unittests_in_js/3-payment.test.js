3ba12269479468c9c72e192d3742b306bb873670
petersudai <81692851+petersudai@users.noreply.github.com>
2024-09-07 22:24:59 +0300

    Spies

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,20 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call Utils.calculateNumber with SUM, 100, and 20', () => {
+    // Create a spy for the Utils.calculateNumber function
+    const spy = sinon.spy(Utils, 'calculateNumber');
+
+    // Call the function
+    sendPaymentRequestToApi(100, 20);
+
+    // Check if the spy was called with the correct arguments
+    expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+
+    // Restore the original function
+    spy.restore();
+  });
+});
